84ef56b59f6b69c62557eedfb8c3b184efd3c774
Apoorv Kothari <apoorv@toidiu.com>
2024-06-27 18:34:37 -0700

    cleanup

--- examples/s2n-mtls/src/bin/quic_echo_client.rs
+++ examples/s2n-mtls/src/bin/quic_echo_client.rs
@@ -1,10 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-use std::{error::Error, net::SocketAddr, path::Path};
-
-pub use s2n_quic::provider::tls::s2n_tls;
-use s2n_quic::{client::Connect, Client};
+use s2n_quic::provider::tls::s2n_tls;
+use std::error::Error;
 
 pub static CA_CERT_PEM: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/certs/ca-cert.pem");
 pub static CLIENT_CERT_PEM: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/certs/client-cert.pem");
@@ -13,37 +11,6 @@ pub static CLIENT_KEY_PEM: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/certs/cl
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     google().await;
-    //let tls = s2n_tls::Client::builder()
-    //    .with_certificate(Path::new(CA_CERT_PEM))?
-    //    .with_client_identity(Path::new(CLIENT_CERT_PEM), Path::new(CLIENT_KEY_PEM))?
-    //    .build()?;
-    //
-    //let client = Client::builder()
-    //    .with_tls(tls)?
-    //    .with_io("0.0.0.0:0")?
-    //    .start()?;
-    //
-    //let addr: SocketAddr = "127.0.0.1:4433".parse()?;
-    //let connect = Connect::new(addr).with_server_name("localhost");
-    //let mut connection = client.connect(connect).await?;
-    //
-    //// ensure the connection doesn't time out with inactivity
-    //connection.keep_alive(true)?;
-    //
-    //// open a new stream and split the receiving and sending sides
-    //let stream = connection.open_bidirectional_stream().await?;
-    //let (mut receive_stream, mut send_stream) = stream.split();
-    //
-    //// spawn a task that copies responses from the server to stdout
-    //tokio::spawn(async move {
-    //    let mut stdout = tokio::io::stdout();
-    //    let _ = tokio::io::copy(&mut receive_stream, &mut stdout).await;
-    //});
-    //
-    //// copy data from stdin and send it to the server
-    //let mut stdin = tokio::io::stdin();
-    //tokio::io::copy(&mut stdin, &mut send_stream).await?;
-
     Ok(())
 }
 
